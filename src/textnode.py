10089014add26c72431ec647182ffc3742f21c2c
Papandoniou Vasileios <vassilis.pap@gmail.com>
2025-03-13 15:21:54 +0200

    1.1 Building extract functions

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import LeafNode
+import re
 
 class TextType(Enum):
     TEXT = "text"
@@ -36,6 +37,43 @@ def text_node_to_html_node(text_node):
         return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url
-                                      , "alt": text_node.text})
+                                    , "alt": text_node.text})
     return ValueError(f"Unknown text type: {text_node.text_type}")
-        
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            new_texts = node.text.split(delimiter)
+            i = 0
+            if len(new_texts) == 1:
+                new_nodes.append(node)
+            else:
+                i = 0
+                for new_text in new_texts:
+                    if new_text != "":
+                        if i%2 == 0:
+                            new_nodes.append(TextNode(new_text, TextType.TEXT))
+                        else:
+                            new_nodes.append(TextNode(new_text, text_type))
+                    i += 1
+        else:
+            new_nodes.append(node)
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    target_tuple = []
+    images = re.findall(r"!\[(.*?)\]", text)
+    links = re.findall(r"\]\((.*?)\)", text)
+    for image in images:
+        target_tuple.append((image, links[images.index(image)]))
+    return target_tuple
+
+def extract_markdown_links(text):
+    target_tuple = []
+    link_texts = re.findall(r"\[(.*?)\]", text)
+    links = re.findall(r"\]\((.*?)\)", text)
+    for link_text in link_texts:
+        target_tuple.append((link_text, links[link_texts.index(link_text)]))
+    return target_tuple
---
+++ src/unittest_test.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+def add_function(a,b):
+    return a + b
+
+class TestAddFunction(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(add_function(1,2),3)
+    def test2(self):
+        self.assertEqual(add_function(1,-7),-6)
+    def test3(self):
+        self.assertEqual(add_function(1,2),3)
+    def test4(self):
+        self.assertEqual(add_function(1,2),3)
+    def test5(self):
+        self.assertEqual(add_function(1,2),3)
+    def test6(self):
+        self.assertEqual(add_function(1,2),3)
+
+
+if __name__ == '__main__':
+    unittest.main()
