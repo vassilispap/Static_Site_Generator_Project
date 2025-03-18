4811e94ca6ce205a4063e563e7af0489d75fed9e
Papandoniou Vasileios <vassilis.pap@gmail.com>
2025-03-19 00:23:07 +0200

    1.4.1 1.4 Markdown_blocks implementation (last one)

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -58,3 +58,9 @@ def block_to_block_type(block):
         return BlockType.ORDERED_LIST
 
     return BlockType.PARAGRAPH
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    html_nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
